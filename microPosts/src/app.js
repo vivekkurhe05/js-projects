8d73b4b81179981b48b149712eaedf685d2cb35b
Vivek Kurhe <vivek.kurhe@launchventures.co>
2022-10-25 13:52:57 +0530

    microposts project

---
+++ microPosts/src/app.js
@@ -0,0 +1,114 @@
+import { http } from "./http";
+import { ui } from "./ui";
+
+// Get posts on DOM load
+document.addEventListener("DOMContentLoaded", getPosts);
+
+// Listen for add post
+document.querySelector(".post-submit").addEventListener("click", submitPost);
+
+// Listen for delete
+document.querySelector("#posts").addEventListener("click", deletePost);
+
+// Listen for edit state
+document.querySelector("#posts").addEventListener("click", enableEdit);
+
+// Listen for cancel edit
+document.querySelector(".card-form").addEventListener("click", cancelEdit);
+
+// Get posts
+function getPosts() {
+  http.get("http://localhost:3000/posts")
+    .then(data => ui.showPosts(data))
+    .catch(err => console.log(err));
+}
+
+// Submit post
+function submitPost() {
+  const title = document.querySelector("#title").value;
+  const body = document.querySelector("#body").value;
+  const id = document.querySelector("#id").value;
+
+
+  // It is as same as the below one, you don't need to specify value if key and value are same
+  const data = {
+    title,
+    body,
+  }
+  // const data = {
+  //   title: title,
+  //   body: body,
+  // }
+
+  // Validate input
+  if(title === "" || body === "") {
+    ui.showAlert("Please fill in all fields", "alert alert-danger");
+  } else {
+    // Check for ID
+    if(id === "") {
+      // Create post
+      http.post("http://localhost:3000/posts", data)
+      .then(data => {
+        ui.showAlert("Post added", "alert alert-success");
+        ui.clearFields();
+        getPosts();
+      })
+      .catch(err =>console.log(err));
+    } else {
+      // Update post
+      http.put(`http://localhost:3000/posts/${id}`, data)
+      .then(data => {
+        ui.showAlert("Post updated", "alert alert-success");
+        ui.changeFormState("add");
+        getPosts();
+      })
+      .catch(err =>console.log(err));
+    }   
+  }
+}
+
+// Delete post
+function deletePost(e) {
+  if(e.target.parentElement.classList.contains("delete")) {
+    const id = e.target.parentElement.dataset.id;
+    if(confirm("Are you sure?")) {
+      http.delete(`http://localhost:3000/posts/${id}`)
+        .then(data => {
+          ui.showAlert("Post removed", "alert alert-success");
+          getPosts();
+        })
+        .catch(err => console.log(err));
+    }
+  }
+  e.preventDefault();
+}
+
+// Enable edit state
+function enableEdit(e) {
+  if(e.target.parentElement.classList.contains("edit")) {
+    const id = e.target.parentElement.dataset.id;
+    const title = e.target.parentElement.parentElement.previousElementSibling.previousElementSibling.textContent;
+    const body = e.target.parentElement.parentElement.previousElementSibling.textContent;
+    
+    const data = {
+      id,
+      title,
+      body
+    }
+
+    // Fill form with current post
+    ui.fillForm(data);
+  }
+  
+
+  e.preventDefault();
+}
+
+// Cancel edit state
+function cancelEdit(e) {
+  if(e.target.classList.contains("post-cancel")) {
+    ui.changeFormState("add");
+  }
+
+  e.preventDefault();
+}
